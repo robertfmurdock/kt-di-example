67a7d90cf9439e5a79098ca66e4cfb7d53af49d6
Robert Murdock <rmurdock@pillartechnology.com>
2019-01-04 15:11:33 -0500

    Made failing unit test pass.

--- src/main/kotlin/kt/di/example/GetLimeAction.kt
+++ src/main/kotlin/kt/di/example/GetLimeAction.kt
@@ -6,7 +6,5 @@ interface GetLimeActionDispatcher {
 
     val limeRepository: LimeRepository
 
-    fun GetLimeAction.perform(): Lime {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    fun GetLimeAction.perform() = limeRepository.get()
 }
