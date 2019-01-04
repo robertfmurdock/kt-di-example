650a096348f0f7b7acc516abab82bca906611607
Robert Murdock <rmurdock@pillartechnology.com>
2019-01-04 15:34:29 -0500

    Refactors.

--- src/main/kotlin/kt/di/example/PutLimeInCoconutAction.kt
+++ src/main/kotlin/kt/di/example/PutLimeInCoconutAction.kt
@@ -4,13 +4,11 @@ data class PutLimeInCoconutAction(val lime: Lime, val coconut: Coconut)
 
 interface PutLimeInCoconutActionDispatcher : CoconutSplitSyntax {
 
-    fun PutLimeInCoconutAction.perform(): HalfCoconut {
-        val halves = coconut.split()
+    fun PutLimeInCoconutAction.perform() = coconut.split()
+            .halfWithMilk()
+            .putIn(lime)
 
-        val halfWithMilk = halves.first { it.milk != null }
-
-        return halfWithMilk.putIn(lime)
-    }
+    private fun List<HalfCoconut>.halfWithMilk() = first { it.milk != null }
 
     private fun HalfCoconut.putIn(lime: Lime) = copy(milk = milk.mix(lime))
 
