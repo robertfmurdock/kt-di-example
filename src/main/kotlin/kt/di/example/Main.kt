124399355239fa61acd1077d467f8e99b43775a7
Robert Murdock <rmurdock@pillartechnology.com>
2019-01-04 19:55:53 -0500

    Minor refactors to injection point.

--- src/main/kotlin/kt/di/example/Main.kt
+++ src/main/kotlin/kt/di/example/Main.kt
@@ -1,28 +1,26 @@
 package kt.di.example
 
-val repositoryDirectory = object : RepositoryDirectory {
-    override val coconutRepository: CoconutRepository get() = SingletonCoconutRepository
-    override val limeRepository: LimeRepository get() = SingletonLimeRepository
-}
-
-val clientDirectory = object : ClientDirectory {
-    override val waiter: Waiter get() = SingletonWaiter
-}
-
-interface CommandDispatcher : MainCommandDispatcher, CoconutCommandDispatcher
-
-val commandDispatcher: CommandDispatcher = object : CommandDispatcher,
-        CoconutStepDispatcher,
-        RepositoryDirectory by repositoryDirectory,
-        ClientDirectory by clientDirectory {
-    override val actionDispatcher: CoconutStepDispatcher get() = this
-}
-
-fun main(args: Array<String>) = commandDispatcher.run {
-
+fun main(args: Array<String>) = with(commandDispatcher()) {
     when {
         args.isEmpty() -> MainCommand().perform()
         args.getOrNull(0) == "coconut" -> CoconutCommand(args[1]).perform()
     }
+}
 
+interface CommandDispatcher : MainCommandDispatcher, CoconutCommandDispatcher
+
+private fun commandDispatcher(): CommandDispatcher = object : CommandDispatcher,
+        CoconutStepDispatcher,
+        RepositoryDirectory by repositoryDirectory(),
+        ClientDirectory by clientDirectory() {
+    override val actionDispatcher: CoconutStepDispatcher get() = this
+}
+
+private fun clientDirectory(): ClientDirectory = object : ClientDirectory {
+    override val waiter: Waiter get() = SingletonWaiter
+}
+
+private fun repositoryDirectory(): RepositoryDirectory = object : RepositoryDirectory {
+    override val coconutRepository: CoconutRepository get() = SingletonCoconutRepository
+    override val limeRepository: LimeRepository get() = SingletonLimeRepository
 }
